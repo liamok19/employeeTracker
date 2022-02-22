d098837e01272356e9559af2b51e9fde83b64e14
Liam Ok <liamokane19@gmail.com>
2022-02-22 13:24:08 +1100

    updated schema.sql with all the fields and values required for each table. Inlcuding what primary keys are required aswell as the foreign keys for what fields need to be joined.

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,7 @@ USE ems_db;
 
 CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL,
-
+    department_name VARCHAR(30) NOT NULL
 );
 
 
@@ -14,11 +13,23 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(6,2) NOT NULL, 
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
 
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL,
-
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
